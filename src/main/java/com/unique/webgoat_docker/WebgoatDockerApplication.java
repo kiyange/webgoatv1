b5d574b9e387e5504f82cb59b5d38a5b51c96f43
kiyange <kiyange@users.noreply.github.com>
2024-10-24 06:48:34 -0700

    init

---
+++ src/main/java/com/unique/webgoat_docker/WebgoatDockerApplication.java
@@ -0,0 +1,13 @@
+package com.unique.webgoat_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebgoatDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebgoatDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unique/webgoat_docker/WebgoatDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unique.webgoat_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebgoatDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
